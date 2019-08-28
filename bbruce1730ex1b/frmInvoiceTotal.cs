2462395b631e6d6781ec0a8e9ceca3fbcb43f600
Brian.Bruce@my.southeastmn.edu <C*lin1109>
2019-08-28 13:47:45 -0500

    Completed Ex1B

--- bbruce1730ex1b/frmInvoiceTotal.cs
+++ bbruce1730ex1b/frmInvoiceTotal.cs
@@ -26,6 +26,13 @@ namespace bbruce1730ex1b
         {
             //txtTotal.Text = "10";
             //txtTotal.ReadOnly = false;
+            txtDiscountAmount.Text = 
+                (Convert.ToDecimal(txtSubtotal.Text) 
+                * Convert.ToDecimal(txtDiscountPercent.Text) / 100).ToString("0.00");
+            txtTotal.Text = 
+                (Convert.ToDecimal(txtSubtotal.Text) 
+                - Convert.ToDecimal(txtDiscountAmount.Text)).ToString("0.00");
+
         }
 
         private void BtnExit_Click(object sender, EventArgs e)
