7648546cfe3b124da4c4c6e3d34d3e126e62dcfc
Brian.Bruce@my.southeastmn.edu <C*lin1109>
2019-12-13 10:43:07 -0600

    completed ex3d

---
+++ bbruce1730ex3d/Ex3dCalculations.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bbruce1730ex3d
+{
+    public class Ex3dCalculations
+    {
+        public static string ArrayToString(int[] values)
+        {
+            StringBuilder sbResult = new StringBuilder(200);
+            foreach (int value in values)
+            sbResult.Append(value.ToString() + ", ");
+            sbResult.ToString();
+            string result = sbResult.ToString();
+            return result.Remove(result.LastIndexOf(", "));
+        }
+
+        public static int ValueCount(int[] values, int searchValue)
+        {
+            int count = 0;
+
+            foreach (int value in values)
+                if (value == searchValue)
+                    count++;
+
+                // loop
+                //     increment counter if current value equals searchValue
+                // return counter
+                return count;
+        }
+
+        public static int RangeCount(int[] values, int searchMin, int searchMax)
+        {
+            // create counter variable
+            // loop
+            //     increment counter if current value between min and max (inclusive)
+            // return counter
+            int count = 0;
+            foreach (int value in values)
+                if (value >= searchMin && value <= searchMax)
+                    count++;
+            return count;
+        }
+    }
+}
---
+++ bbruce1730ex3dUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("bbruce1730ex3dUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("bbruce1730ex3dUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2a7062b3-0231-4717-a00d-f3f32765858d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
